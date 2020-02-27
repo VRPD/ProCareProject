ae9f8ce0c9fe7f8d759b979558adaed2a84a5230
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-02-27 11:39:00 +0100

    Added a simple tag & script with event that activates once the object collides with a given tag, which we use to teleport the player for now.

---
+++ Assets/Scripts/ActivateOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActivateOnCollision : MonoBehaviour
+{
+	[SerializeField]
+	private string tagToActivate;
+
+	[SerializeField]
+	private UnityEvent activatedEvent;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == tagToActivate)
+		{
+			activatedEvent.Invoke();
+		}
+	}
+}
