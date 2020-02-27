acfbb746f134d851bd9bcff0c964094e30971d8c
VRPD <experion17@gmail.com>
2020-02-27 09:48:42 +0000

    Added script to teleport player
    Scripted added that teleport player to new position when object is picked up.

---
+++ Assets/Scripts/PlayerTeleporter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject node;
+
+
+
+    public void teleportPlayer()
+    {
+        player.transform.position = node.transform.position;
+    }
+}
