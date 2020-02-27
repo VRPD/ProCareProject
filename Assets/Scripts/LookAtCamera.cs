ff1fd919f395106062e31c6eee56bae6f5b9da68
VRPD <experion17@gmail.com>
2020-02-27 09:12:41 +0000

    All of the project from a previous repository.

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProCare.Sim.Utilities
+{
+	public class LookAtCamera : MonoBehaviour
+	{
+		[SerializeField]
+		private Camera target;
+
+		private void Update()
+		{
+			transform.LookAt(target.transform);
+		}
+	}
+}
