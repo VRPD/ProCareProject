af2b74635ccc2cd27d041c0e3d41e0e4c1f0162a
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-03-11 14:33:46 +0100

    Added script that plays an additional sound on top of another, ideal for music songs to adapt on player input.

---
+++ Assets/Scripts/LayeredAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayeredAudio : MonoBehaviour
+{
+	[SerializeField]
+	private AudioSource _baseSound;
+	[SerializeField]
+	private AudioSource _layeredSound;
+
+	public void StartLayeredSound(bool stopBaseSound)
+	{
+		if (_baseSound.isPlaying)
+		{
+			_layeredSound.Play();
+			_layeredSound.time = _baseSound.time;
+
+			if (stopBaseSound) _baseSound.Stop();
+		}
+	}
+
+	public void StopLayeredSound()
+	{
+		_layeredSound.Stop();
+	}
+}
