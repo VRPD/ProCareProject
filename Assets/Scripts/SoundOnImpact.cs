aed4b1c721c78c539436f0341659a2bdc01fb24f
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-03-25 16:34:04 +0100

    Added impact sounds to most objects & tweaked some sounds to have no delay

---
+++ Assets/Scripts/SoundOnImpact.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SoundOnImpact : MonoBehaviour
+{
+	[SerializeField]
+	private AudioSource _audioSource;
+	[SerializeField, Range(0f, 2f)]
+	private float _pitchVariance = 0.2f;
+	[SerializeField]
+	private bool useCollisionStay = true;
+
+
+	private float _pitchStart;
+
+	private void Awake()
+	{
+		_pitchStart = _audioSource.pitch;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		float audioVolume = collision.relativeVelocity.magnitude / 10f;
+		_audioSource.volume = audioVolume;
+		_audioSource.pitch = _pitchStart + Random.Range(-_pitchVariance, _pitchVariance);
+		_audioSource.Play();
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		if (useCollisionStay && ((collision.relativeVelocity.magnitude / 10f) >= _audioSource.volume || !_audioSource.isPlaying))
+		{
+			float audioVolume = collision.relativeVelocity.magnitude / 10f;
+			_audioSource.volume = audioVolume;
+			_audioSource.pitch = _pitchStart + Random.Range(-_pitchVariance, _pitchVariance);
+			_audioSource.Play();
+		}
+	}
+}
