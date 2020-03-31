a347f2f11ce3556683cd4a0f26508f010d21f836
Adrian <adrianvermeij@gmail.com>
2020-03-31 12:45:10 +0200

    Added SteamAudio
    Added SteamAudio to the project for Spatial Audio, added simple script to play the first set of narration when the player enters the level.

---
+++ Assets/Scripts/PlayNarration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayNarration : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource narratePlayer;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        narratePlayer.GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        narratePlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
