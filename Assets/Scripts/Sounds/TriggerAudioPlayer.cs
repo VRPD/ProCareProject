830b519db42248683f661e85468938e2f00ef501
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-04-17 12:04:39 +0200

    First version of UI (no sound), moved some scripts around to more appropriate folders & removed that pesky readme file that always messes up the hierachy view.

---
+++ Assets/Scripts/Sounds/TriggerAudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudioPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private PlayNarration audioManager;
+
+    public int clipNo;
+
+    private void Start()
+    {
+        audioManager = GetComponentInParent<PlayNarration>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+		Debug.Log(other.name);
+        if (other.gameObject.tag == "Player")
+        {
+            audioManager.PlayNextClip(clipNo);
+        }
+    }
+}
---
+++ Assets/Scripts/Sounds/VolumeAdjustment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VolumeAdjustment : MonoBehaviour
+{
+	[SerializeField, Header("References")]
+	private AudioSource[] _volumeSources;
+	[SerializeField]
+	private TMPro.TextMeshProUGUI _percentage;
+
+	[SerializeField, Header("Variables")]
+	private float _volume = 1;
+
+	[SerializeField, Header("Events")]
+	private UnityEvent<float> OnVolumeChange;
+
+	public void IncreaseVolume(float increase)
+	{
+		_volume += increase;
+
+		if (_volume > 1) _volume = 1;
+		if (_percentage) UpdateText();
+
+		OnVolumeChange.Invoke(_volume);
+	}
+
+	public void DecreaseVolume(float decrease)
+	{
+		_volume -= decrease;
+
+		if (_volume < 0) _volume = 0;
+		if (_percentage) UpdateText();
+
+		OnVolumeChange.Invoke(_volume);
+	}
+
+	public void UpdateText()
+	{
+		_percentage.text = (_volume * 100) + "%";
+	}
+
+}
