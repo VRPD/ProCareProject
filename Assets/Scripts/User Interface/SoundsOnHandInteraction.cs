f1abcaf26ec5e69b8a7c720bfaad39d6d43f72ed
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-04-17 16:52:26 +0200

    Added sounds when hovering & clicking a button on the watch.

---
+++ Assets/Scripts/User Interface/SoundsOnHandInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(UIElementExpanded))]
+public class SoundsOnHandInteraction : MonoBehaviour
+{
+	[SerializeField]
+	private UISoundPlayer _player;
+	
+    void Awake()
+    {
+		UIElementExpanded uiStuff = GetComponent<UIElementExpanded>();
+
+		uiStuff.onHandHover.AddListener(OnHandHover);
+		uiStuff.onHandClick.AddListener(OnHandClick);
+	}
+
+	private void OnHandClick(Hand arg0)
+	{
+		_player.PlayClickSound();
+	}
+
+	private void OnHandHover(Hand arg0)
+	{
+		_player.PlayHoverSound();
+	}
+}
---
+++ Assets/Scripts/User Interface/UISoundPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISoundPlayer : MonoBehaviour
+{
+	[SerializeField]
+	private AudioSource _source;
+
+	[SerializeField]
+	private AudioClip _soundOnHover;
+
+	[SerializeField]
+	private AudioClip _soundOnClick;
+	
+	public void PlayHoverSound()
+	{
+		_source.clip = _soundOnHover;
+		_source.Play();
+	}
+
+	public void PlayClickSound()
+	{
+		_source.clip = _soundOnClick;
+		_source.Play();
+	}
+}
