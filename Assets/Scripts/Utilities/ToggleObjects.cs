d16d8e55a8c14cd32c7a15c8c2d8962eee92c2e9
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-05-18 11:22:16 +0200

    Final tweaks for the demo for sprint 3

---
+++ Assets/Scripts/Utilities/ToggleObjects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ToggleObjects : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject[] _gameObjects;
+	public SteamVR_Action_Boolean toggleAction;
+	public SteamVR_Input_Sources handType;
+
+	void Start()
+	{
+		toggleAction.AddOnStateDownListener(TriggerDown, handType);
+	}
+
+	public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+	{
+		for (int i = 0; i < _gameObjects.Length; i++)
+		{
+			_gameObjects[i].SetActive(!_gameObjects[i].activeSelf);
+		}
+	}
+}
