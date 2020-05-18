b4c15ab93df5dc45d7f799806e771d0e51d08724
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-05-18 15:12:13 +0200

    Added a different sound clip for the OnHover event, as well as added minor pitch variants to combat the repetitiveness.

--- Assets/Scripts/User Interface/UISoundPlayer.cs
+++ Assets/Scripts/User Interface/UISoundPlayer.cs
@@ -12,16 +12,21 @@ public class UISoundPlayer : MonoBehaviour
 
 	[SerializeField]
 	private AudioClip _soundOnClick;
+
+	[SerializeField, Range(0, 0.5f)]
+	private float _pitchVariance = 0.1f;
 	
 	public void PlayHoverSound()
 	{
 		_source.clip = _soundOnHover;
+		_source.pitch = 1 + Random.Range(-_pitchVariance, _pitchVariance);
 		_source.Play();
 	}
 
 	public void PlayClickSound()
 	{
 		_source.clip = _soundOnClick;
+		_source.pitch = 1 + Random.Range(-_pitchVariance, _pitchVariance);
 		_source.Play();
 	}
 }
