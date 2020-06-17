6279eb662df1697dd6ca126c2de68577904ee2d8
Adrian <adrianvermeij@gmail.com>
2020-06-17 18:40:51 +0200

    Added narration for tutorial room
    Narration now plays when entering the experience and after completing instructions

---
+++ Assets/ObjectMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    public int yTarget, speed;
+
+    public void MoveCaller()
+    {
+        StartCoroutine(moveObject());
+    }
+
+    IEnumerator moveObject()
+    {
+        while (gameObject.transform.position.y < yTarget)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+
+            yield return null;
+        }
+
+    }
+}
